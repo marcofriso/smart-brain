b62f06036f1c3782afbb9bfd600ec53c677c1e8f
Marco F <marco.friso@gmail.com>
2020-12-23 19:46:30 +0100

    feat: add AWS Lambda ranking system

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: "",
+    };
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.entries === this.props.entries) {
+      return null;
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (count) => {
+    fetch(
+      `https://gjx30zuhi2.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${count}`
+    )
+      .then((response) => response.json())
+      .then((data) => this.setState({ emoji: data.input }))
+      .catch(console.log);
+  };
+
+  render() {
+    const { name, entries } = this.props;
+    return (
+      <div>
+        <div className="white f3">
+          {`${name}, your current entry count is...`}
+        </div>
+        <div className="white f1">{entries}</div>
+        <div className="white f3">{`Rank badge: ${this.state.emoji}`}</div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Rank;
